4c62d0eacfbdb826d7382c754d7f6857f37563df
Amanda Abrams <aca62997@gmail.com>
2022-01-05 15:29:54 -0500

    redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, FormGroup, Form, Label, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -163,6 +164,26 @@ function RenderComments({ comments, addComment, campsiteId }) {
 
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
     if (props.campsite) {
         console.log("This is working");
         return (
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
+import { campsitesFailed } from "../redux/ActionCreators";
 
 function RenderDirectoryItem({campsite}) {
   return (
@@ -16,7 +18,7 @@ function RenderDirectoryItem({campsite}) {
 }
 
 function Directory(props) {
-  const directory = props.campsites.map((campsite) => {
+  const directory = props.campsites.campsites.map((campsite) => {
     return (
       <div key={campsite.id} className="col-md-5 m-1">
         <RenderDirectoryItem campsite={campsite} />
@@ -24,21 +26,41 @@ function Directory(props) {
     );
   });
 
+  if (props.campsites.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.campsites.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.campsites.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container">
       <div className="row">
         <div className="col">
           <Breadcrumb>
-            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
             <BreadcrumbItem active>Directory</BreadcrumbItem>
           </Breadcrumb>
           <h2>Directory</h2>
           <hr />
         </div>
       </div>
-      <div className="row">
-          {directory}
-      </div>
+      <div className="row">{directory}</div>
     </div>
   );
   
