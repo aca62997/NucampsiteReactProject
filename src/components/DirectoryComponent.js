2dee174511c6cfcd527818faa3a43a9d0a9e4bdd
Amanda Abrams <aca62997@gmail.com>
2022-01-07 10:41:59 -0500

    react transitions

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -9,7 +9,7 @@ function RenderDirectoryItem({campsite}) {
   return (
     <Card>
       <Link to={`/directory/${campsite.id}`}>
-        <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
+        <CardImg width="100%" src={baseUrl +  campsite.image} alt={campsite.name} />
         <CardImgOverlay>
           <CardTitle>{campsite.name}</CardTitle>
         </CardImgOverlay>
